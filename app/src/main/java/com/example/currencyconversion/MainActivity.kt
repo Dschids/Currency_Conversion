22a972af58def2b422e49d833183325462afddff
Bob Dschida <rdschida@students.mchenry.edu>
2023-10-26 16:52:16 -0500

    it works and has comments now

--- app/src/main/java/com/example/currencyconversion/MainActivity.kt
+++ app/src/main/java/com/example/currencyconversion/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.currencyconversion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioButton
@@ -13,25 +14,42 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val edtTextNumToConvert = findViewById<EditText>(R.id.idEditTextDecimalToConvert)
+        // set up variables for all of the different textviews and buttons
+        val numToConvert = findViewById<EditText>(R.id.idEditTextDecimalToConvert)
         val rbUsToEu = findViewById<RadioButton>(R.id.idRbUsToEu)
         val rbEuToUs = findViewById<RadioButton>(R.id.idRbEuToUs)
         val btnConvert = findViewById<Button>(R.id.idBtnConvert)
         val txtResult = findViewById<TextView>(R.id.idTxtConvertedAmount)
 
-        val convertIt = edtTextNumToConvert.text.toString().toDouble()
-        val conversionRate = 0.94
-        val decim = DecimalFormat("###,###.00")
-        var converted : Double?
-
-
+        // set an on click listener for our button
         btnConvert.setOnClickListener {
+            // take input from the edit text box for amount to convert and change it to a string
+            var convertString = numToConvert.text.toString()
+            // create a variable to hold the double value of the input edit text
+            var convertIt = 0.00
+            // make sure that the edit text has something in it
+            if (TextUtils.isEmpty(convertString)){
+                // convert our previous string into a double
+                numToConvert.error = "Error\nAmount to convert is blank."
+            } else {
+                convertIt = convertString.toDouble()
+
+            }
+            // variables for the conversion rate, a decimal format and a blank variable for our converted number
+            val conversionRate = 0.94
+            val euroFormat = DecimalFormat("\u20ac ###,##0.00")
+            val dolFormat = DecimalFormat("$ ###,##0.00")
+            var converted : Double?
+
+            /*
+            check which radio button is checked and do math based on which is
+            nested if checks if number entered is greater than 10,000 and prompts user to enter a smaller number
+            if it is.
+             */
             if (rbUsToEu.isChecked){
                 if (convertIt <= 10000){
                     converted = convertIt * conversionRate
-                    var euros = decim.format(converted) + " Euros"
-                    txtResult.text = euros
+                    txtResult.text = euroFormat.format(converted)
                 } else {
                     Toast.makeText(this@MainActivity, "Dollars must be less than 10,000",Toast.LENGTH_LONG).show()
                 }
@@ -39,8 +57,7 @@ class MainActivity : AppCompatActivity() {
             if (rbEuToUs.isChecked){
                 if (convertIt <= 10000){
                     converted = convertIt / conversionRate
-                    var dollars = decim.format(converted) + " Dollars"
-                    txtResult.text = dollars
+                    txtResult.text = dolFormat.format(converted)
                 } else {
                     Toast.makeText(this@MainActivity, "Euros must be less than 10,000",Toast.LENGTH_LONG).show()
                 }
